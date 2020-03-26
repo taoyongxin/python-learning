b2d9a1aef4dde6182d4402ce4c3549f846c602f4
taoyongxin <1427177855@qq.com>
2020-03-26 17:37:31 +0800

    python-

---
+++ jd-test/goods.py
@@ -0,0 +1,9 @@
+class Goods(object):
+
+    self.size = 0
+
+    def __self__(self, price=0.0, title='', page=''):
+        self.price = 0.0
+        self.title = title
+        self.page = page
+        self.size = self.size + 1
---
+++ jd-test/parse.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+
+
+def parse_data(body):
+    soup = BeautifulSoup(body)
+    return None
