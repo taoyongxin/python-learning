13acf8a66805a35270b84ad372b96e320dd9c1f4
taoyongxin <1427177855@qq.com>
2020-05-24 21:59:31 +0800

    python day-61

--- 系统学习/Day60/NumPy数值计算练习起步.py
+++ 系统学习/Day60/NumPy数值计算练习起步.py
@@ -3,14 +3,14 @@ NumPy数值计算练习起步
 """
 import numpy as np
 
-# 通过构造函数array创建亿维array
+# 通过构造函数array 创建一维array
 v = np.array([1, 2, 3, 4])
 print(v)
 
 # 创建二维array
 m = np.array([[1, 2], [3, 4]])
 print(m)
-# v和m的类型都是ndarray,NumPy中最主要的数据结构
+# v 和 m 的类型都是 ndarray, NumPy 中最主要的数据结构
 print(type(v), type(m))
 
 # 指定范围内的数组
